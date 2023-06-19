8d38744145a375c13ed8767d1909f14fc8e7cb29
Nightishes <q.bathany@gmail.com>
2023-06-19 11:18:54 +0200

    Cors fix?

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const cors = require(cors)
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
@@ -15,12 +16,7 @@ const normalizePort = val => {
 const port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
+app.use(cors());
 
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
