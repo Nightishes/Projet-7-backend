dee13c7622289006e8739c17151e1e4936ac9367
Nightishes <q.bathany@gmail.com>
2023-06-19 12:06:21 +0200

    again cors

--- src/app.js
+++ src/app.js
@@ -15,12 +15,6 @@ mongoose.connect('mongodb+srv://qbathany:oQIsdlLzU1YsiTG8@cluster0.i1fenpo.mongo
 
 
 app.use(express.json()); 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
 
 
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const http = require('http');
 const app = require('./app');
-const cors = require(cors)
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
