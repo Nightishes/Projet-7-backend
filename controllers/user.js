e3e3775c404ce3547923ef434a8c86f460e1ac24
Nightishes <q.bathany@gmail.com>
2023-06-21 11:07:45 +0200

    change bcrypt

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
