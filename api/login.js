4205dea4706e6c709ac87c81a3100ec921083d4b
Nightishes <q.bathany@gmail.com>
2023-06-24 17:06:33 +0200

    folder issue?

--- api/login.js
+++
@@ -1,6 +0,0 @@
-const app = require('../app')
-const route = require('../routes/users')
-
-app.use("/api/auth/login", route)
-
-module.exports = app;
--- api/signup.js
+++
@@ -1,6 +0,0 @@
-const app = require('../app')
-const route = require('../routes/users')
-
-app.use("/api/auth/signup", route)
-
-module.exports = app;
